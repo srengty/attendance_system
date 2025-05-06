18462195853f1bd83044fc0be25266db85dadd21
srengty <srengty@gmail.com>
2025-05-06 08:02:53 +0700

    first commit

---
+++ src/test/java/gic/i4/attendance_system/AttendanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package gic.i4.attendance_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
